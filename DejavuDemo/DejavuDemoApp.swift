55ab26d9949d9213bb33c7bb3a9f1b8744374f90
Harish Kumar Reddy Kunchala <hkunchala@esri.com>
2024-11-26 10:17:36 -0800

    Initial Commit

---
+++ DejavuDemo/DejavuDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  DejavuDemoApp.swift
+//  DejavuDemo
+//
+//  Created by Harish Kumar Reddy Kunchala on 11/26/24.
+//
+
+import SwiftUI
+
+@main
+struct DejavuDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
